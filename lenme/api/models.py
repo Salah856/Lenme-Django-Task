22944b25c308d70bb586b04a235f03ebe27a8092
salah856 <salah_elhossiny856@yahoo.com>
2021-11-15 23:11:34 +0200

    add migrations and  refactor api models

--- lenme/api/models.py
+++ lenme/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 
 # Create your models here.
 
@@ -11,23 +11,23 @@ LOAN_STATUS = (
 
 
 class Investor(models.Model):
-    investor_name = models.CharField(max_length=100, required=True)
+    investor_name = models.CharField(max_length=100, blank=True)
     investor_id = models.CharField(primary_key=True, max_length=100)
-    balance = models.IntegerField(default=0, required=True)
+    balance = models.IntegerField(default=0, blank=True)
 
 
 class Borrower(models.Model):
-    borrower_name = models.CharField(max_length=100, required=True)
+    borrower_name = models.CharField(max_length=100, blank=True)
     borrower_id = models.CharField(primary_key=True, max_length=100)
 
 
 class Loan(models.Model):
     loan_id = models.CharField(primary_key=True, max_length=100)
-    loan_amount = models.IntegerField(default=0, required=True)
+    loan_amount = models.IntegerField(default=0, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     loan_period = models.DateTimeField(
-            dt.now() + dt.timedelta(days=180)
+            dt.now() + timedelta(days=180)
         )  # 6 months
     loan_status = models.CharField(
             choices=LOAN_STATUS,
