eda36cfb77823db1273f1ddc74cea957bda232fe
salah856 <salah_elhossiny856@yahoo.com>
2021-11-15 22:06:33 +0200

    refactor models

--- lenme/api/models.py
+++ lenme/api/models.py
@@ -11,25 +11,29 @@ LOAN_STATUS = (
 
 
 class Investor(models.Model):
-    investorname = models.CharField(max_length=100)
+    investor_name = models.CharField(max_length=100, required=True)
     investor_id = models.CharField(primary_key=True, max_length=100)
-    balance = models.IntegerField(default=0)
+    balance = models.IntegerField(default=0, required=True)
 
 
 class Borrower(models.Model):
-    borrowername = models.CharField(max_length=100)
+    borrower_name = models.CharField(max_length=100, required=True)
     borrower_id = models.CharField(primary_key=True, max_length=100)
 
 
 class Loan(models.Model):
     loan_id = models.CharField(primary_key=True, max_length=100)
-    loan_amount = models.IntegerField(default=0)
+    loan_amount = models.IntegerField(default=0, required=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     loan_period = models.DateTimeField(
             dt.now() + dt.timedelta(days=180)
         )  # 6 months
-    loan_status = models.CharField(choices=LOAN_STATUS, max_length=100)
+    loan_status = models.CharField(
+            choices=LOAN_STATUS,
+            max_length=100,
+            default=LOAN_STATUS[0][0]
+        )
     borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)
     investor = models.ForeignKey(Investor, on_delete=models.CASCADE)
 
