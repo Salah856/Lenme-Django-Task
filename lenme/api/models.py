67946418b360ad40699667d0369c562535f7f43e
salah856 <salah_elhossiny856@yahoo.com>
2021-11-15 22:00:23 +0200

    added models of lenme

--- lenme/api/models.py
+++ lenme/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime as dt
 
 # Create your models here.
+
+LOAN_STATUS = (
+    ('Requested', 'Requested'),
+    ('Funded', 'Funded'),
+    ('Completed', 'Completed'),
+)
+
+
+class Investor(models.Model):
+    investorname = models.CharField(max_length=100)
+    investor_id = models.CharField(primary_key=True, max_length=100)
+    balance = models.IntegerField(default=0)
+
+
+class Borrower(models.Model):
+    borrowername = models.CharField(max_length=100)
+    borrower_id = models.CharField(primary_key=True, max_length=100)
+
+
+class Loan(models.Model):
+    loan_id = models.CharField(primary_key=True, max_length=100)
+    loan_amount = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    loan_period = models.DateTimeField(
+            dt.now() + dt.timedelta(days=180)
+        )  # 6 months
+    loan_status = models.CharField(choices=LOAN_STATUS, max_length=100)
+    borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)
+    investor = models.ForeignKey(Investor, on_delete=models.CASCADE)
+
+
