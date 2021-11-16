92347d07f2bd90b73a29a69488c747df6fb90171
salah856 <salah_elhossiny856@yahoo.com>
2021-11-16 08:16:49 +0200

    refactor utils function

--- lenme/api/utils.py
+++ lenme/api/utils.py
@@ -1,3 +1,6 @@
+from .models import Investor
+
+
 def check_investor_balance(investor_id, loan):
     investor = Investor.objects.get(pk=investor_id)
     return investor.balance > loan.loan_amount + loan.lenme_fee
