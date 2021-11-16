c76b4f5fa85709e227a115e7b4b1cedf7e116b5d
salah856 <salah_elhossiny856@yahoo.com>
2021-11-16 07:58:05 +0200

    added views controllers in api urls

--- lenme/api/urls.py
+++ lenme/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import hello
+from .views import request_loan, submit_loan_offer, accept_loan
 
 
 urlpatterns = [
-    path('hello/', hello),
+    path('request_loan/', request_loan, name='request_loan'),
+    path('submit_loan_offer/', submit_loan_offer, name='submit_loan_offer'),
+    path('accept_loan/', accept_loan, name='accept_loan'),
 ]
